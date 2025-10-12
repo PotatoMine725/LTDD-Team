e8073d6ce8cf9387c2d70d5648d9ed1ed9bf20da
Le Khac Hieu <lekhachieupt@gmail.com>
2025-10-12 18:32:43 +0700

    pull from main

--- EnglishApp/app/src/main/java/com/example/englishapp/MainActivity.java
+++ EnglishApp/app/src/main/java/com/example/englishapp/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         final int initialPaddingBottom = mainView.getPaddingBottom();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             v.setPadding(
                     initialPaddingLeft + systemBars.left,
                     initialPaddingTop + systemBars.top,
