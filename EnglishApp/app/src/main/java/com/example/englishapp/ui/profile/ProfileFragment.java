daeddc2e76c1cf09602c39ddf5ef609adecd4bbc
nhatminh104 <hanhatminh1042005@gmail.com>
2025-12-22 14:55:10 +0700

    fix logout profile

--- EnglishApp/app/src/main/java/com/example/englishapp/ui/profile/ProfileFragment.java
+++ EnglishApp/app/src/main/java/com/example/englishapp/ui/profile/ProfileFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.englishapp.R;
+import com.example.englishapp.ui.auth.GetStartedActivity;
 import com.example.englishapp.ui.auth.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -124,11 +125,15 @@ public class ProfileFragment extends Fragment {
     private void logout() {
         auth.signOut();
 
-        Intent intent = new Intent(requireActivity(), LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        Intent intent = new Intent(requireActivity(), GetStartedActivity.class);
+        intent.addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK |
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK
+        );
         startActivity(intent);
         requireActivity().finish();
     }
+
 
     // ================= CLEAN LISTENER =================
 
